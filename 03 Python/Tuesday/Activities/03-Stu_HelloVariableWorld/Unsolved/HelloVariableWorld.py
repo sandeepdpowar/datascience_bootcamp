3eede1e91cf09069ec7295fd4fcc98dba43dc7b1
Sandeep Powar <cyan.entrepreneur@gmail.com>
2022-05-18 20:30:14 -0400

    All stock Analysis done

---
+++ 03 Python/Tuesday/Activities/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -0,0 +1,32 @@
+# Create a variable called 'name' that holds a string
+name = "Sandeep"
+
+# Create a variable called 'country' that holds a string
+country = "Canada"
+
+# Create a variable called 'age' that holds an integer
+age = 35
+
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 85.5
+
+# Calculate the daily wage for the user
+daily_wage = hourly_wage * 8
+
+# Create a variable called 'satisfied' that holds a boolean
+satisfied = truw
+
+# Print out "Hello <name>!"
+print(f"Hello {name}")
+
+# Print out what country the user entered
+print(f"Country {country}")
+
+# Print out the user's age
+print(f"Age {age}")
+
+# With an f-string, print out the daily wage that was calculated
+print(f"Daily wage {daily_wage}")
+
+# With an f-string, print out whether the users were satisfied
+print(f"User was satisfied with the wage {satisfied}")
---
+++ 03_Python/Tuesday/Activities/05-Stu_GroceryList/Unsolved/grocery_list.py
@@ -0,0 +1,17 @@
+# Create a Python list to store your grocery list
+newlist = ["Oranges", "Peanut Butter", "Cheese", "Jelly"]
+
+# Print the grocery list
+print(newlist)
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+
+newlist[1] = "Almond Butter"
+print(newlist)
+# Remove "Jelly" from grocery list and print out the updated list
+
+newlist.Remove("Jelly")
+print(newlist)
+
+# Add "Coffee" to grocery list and print the updated list
+newlist.Append("Coffee")
+print(newlist)
